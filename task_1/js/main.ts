de977ff8a966291d47c71bcb697f740a50da26fa
Richmond Cowther <richmondcowther@gmail.com>
2025-09-11 22:54:57 +0000

    Update main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -15,4 +15,21 @@ const teacher: Teacher = {
   contract: true
 };
 
+
+// Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: "Alice",
+  lastName: "Johnson",
+  fullTimeEmployee: true,
+  location: "Accra",
+  numberOfReports: 5,
+  contract: true
+};
+
+console.log(director1);
 console.log(teacher); 
+
