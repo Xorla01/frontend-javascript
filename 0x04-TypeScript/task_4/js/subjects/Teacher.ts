934335eca82223876b7c504da26fea24edbfe973
Richmond Cowther <richmondcowther@gmail.com>
2025-09-13 19:32:54 +0000

    0x04-TypeScript/task_4/js/subjects directory

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++
@@ -1,9 +0,0 @@
-namespace Subjects {
-  export interface Teacher {
-    firstName: string;
-    lastName: string;
-    experienceTeachingC: number;      // always required
-    experienceTeachingReact: number;  // always required
-    experienceTeachingJava: number;   // always required
-  }
-}
