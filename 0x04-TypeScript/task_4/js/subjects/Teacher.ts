61c53fe172a9e3891338c16a925e176435bcda8e
Richmond Cowther <richmondcowther@gmail.com>
2025-09-13 19:25:02 +0000

    Add files via upload

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,9 @@
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+    experienceTeachingC: number;      // always required
+    experienceTeachingReact: number;  // always required
+    experienceTeachingJava: number;   // always required
+  }
+}
